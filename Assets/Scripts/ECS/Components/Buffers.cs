5fe863ce3400f88ec18b50ea505b2c80d7ec7596
antho7575 <anthony_dimenna@outlook.com>
2025-10-17 18:36:35 -0400

    Clean up code

--- Assets/Scripts/ECS/Components/Buffers.cs
+++ Assets/Scripts/ECS/Components/Buffers.cs
@@ -1,7 +1,3 @@
 using Unity.Entities;
-using Unity.Mathematics;
 
 [InternalBufferCapacity(0)] public struct CellCost : IBufferElementData { public ushort Value; }
-[InternalBufferCapacity(0)] public struct CellIntegration : IBufferElementData { public uint Value; }
-[InternalBufferCapacity(0)] public struct CellDir : IBufferElementData { public float2 Value; }
-[InternalBufferCapacity(8)] public struct InventoryItem : IBufferElementData { public int ItemId; public int Count; }
